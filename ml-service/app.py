4627b8c4b699926efce9f8695601af851845e21c
sahilwasa444 <sahilwasalwar@gmail.com>
2026-03-06 19:42:25 +0530

    deploy: latest changes

--- ml-service/app.py
+++ ml-service/app.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500","https://subal-healthcare-platform.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
